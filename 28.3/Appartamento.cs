26de3104a64fbbce48b0c8365bfae1670ea8e93a
casta <casta@ALICELABELLA>
2023-11-30 18:24:54 +0100

    Aggiungere i file di progetto.

---
+++ 28.3/Appartamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._3
+{
+    internal class Appartamento: immobile
+    {
+        public int Numerovani { get; set; }
+        public int Numerobagni { get; set; }
+
+    }
+}
---
+++ 28.3/immobile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _28._3
+{
+    internal class immobile:Agenzia
+    {
+        public string Codice { get; set; }
+        public string Indirizzo { get; set; }
+        public string Cap { get; set; }
+        public string città { get; set; }
+        public  int Superficie { get; set; }
+
+    }
+}
